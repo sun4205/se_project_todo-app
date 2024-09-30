9024d153b2c32193b28d2c6a9c938a7a2a9989dd
sun4205 <hanna4205@gmail.com>
2024-09-30 03:50:57 -0400

    totocounter

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js";
+import TodoCounter from "../components/TodoCounter.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopupEl = document.querySelector("#add-todo-popup");
@@ -11,6 +12,9 @@ const addTodoForm = addTodoPopupEl.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopupEl.querySelector(".popup__close");
 const todoTemplate = document.querySelector("#todo-template");
 const todosList = document.querySelector(".todos__list");
+
+const todoCounter = new TodoCounter(initialTodos,".counter__text");
+
 
 const addTodoPopup = new PopupWithForm({
   popupSelector: "#add-todo-popup",
